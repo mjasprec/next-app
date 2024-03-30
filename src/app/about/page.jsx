e3baaed4b21fb5c4cee92c42bd626c4bc9d5d556
MJAsprec <michael.asprec@whitecloak.com>
2024-03-30 16:01:32 +0800

    update: working with next image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+import styles from './about.module.css';
+
 const AboutPage = () => {
-  return <div>AboutPage!</div>;
+  return (
+    <div>
+      <div className={styles.imageContainer}>
+        <Image
+          // src='/about.png'
+          src='https://images.pexels.com/photos/7589354/pexels-photo-7589354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          alt='about us'
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
